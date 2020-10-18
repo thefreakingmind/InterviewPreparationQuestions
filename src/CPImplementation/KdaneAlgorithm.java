d736875732456dd8dfa9266625538a694671d065
thefreakingmind12 <salmansiddiqui100@gmail.com>
2020-10-18 12:11:29 +0530

    Updated DP

--- src/CPImplementation/KdaneAlgorithm.java
+++ src/CPImplementation/KdaneAlgorithm.java
@@ -1,14 +1,22 @@
 package CPImplementation;
 
-import java.util.ArrayDeque;
+import java.util.*;
 
 public class KdaneAlgorithm {
     static class KdaneAlgo{
-        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+
 
 
     }
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Scanner in = new Scanner(System.in);
+        Map<String, String> map = new HashMap<>();
+
+
 
     }
 }
---
+++ src/DynamicProgramming/MaximumSubarray.java
@@ -0,0 +1,25 @@
+package DynamicProgramming;
+
+import java.util.*;
+
+public class MaximumSubarray {
+    private StringBuilder helperFunction = new StringBuilder();
+
+
+    public static void main(String[] args) {
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] dp = new int[n+1];
+        int[] handler = new int[n+1];
+        int[] arr = new int[n+1];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 1){
+
+            }
+        }
+
+    }
+}
